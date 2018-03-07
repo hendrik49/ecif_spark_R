720690e08dd66a38c614ac597c160c8317173558
didok49 <drikdoank@gmail.com>
2018-03-07 16:18:44 +0700

    pindah

--- ecif.R
+++ ecif.R
@@ -12,25 +12,45 @@ config$`sparklyr.shell.driver-class-path` <-
 
 sc <- spark_connect(master = "local", app_name = "ECIF SPark Engine",  config = config)
 
-individualgoldendata <- spark_read_jdbc(sc, "ecif_jdbc",  options = list(
+individualgoldendata <- spark_read_jdbc(sc, "ecif_golden_jdbc",  options = list(
   url = "jdbc:postgresql://localhost:5432/ECIF",
   user = "postgres",
   password = "postgres",
   dbtable = "public.individualgoldendata"))
 
-individualgoldendatahistory <- spark_read_jdbc(sc, "ecif_jdbc",  options = list(
+individualgoldendatahistory <- spark_read_jdbc(sc, "ecif_golden_history_jdbc",  options = list(
   url = "jdbc:postgresql://localhost:5432/ECIF",
   user = "postgres",
   password = "postgres",
   dbtable = "public.individualgoldendatahistory"))
 
-individualdatasteward <- spark_read_jdbc(sc, "ecif_jdbc",  options = list(
+individualdatasteward <- spark_read_jdbc(sc, "ecif_steward_jdbc",  options = list(
   url = "jdbc:postgresql://localhost:5432/ECIF",
   user = "postgres",
   password = "postgres",
   dbtable = "public.individualdatasteward"))
 
+object.size(individualgoldendata)
+object.size(individualgoldendatahistory)
+object.size(individualdatasteward)
 
-test <- dbGetQuery(sc, "SELECT * FROM public.individualgoldendata LIMIT 10")
-test
+join_data <- inner_join(individualgoldendata, individualgoldendatahistory, by = c("ECIF" = "ECIFRef"))
+count(join_data)
+
+data = join_data %>% 
+  group_by(ECIF) %>%
+  summarise(total = n())  %>% 
+  filter(total >= 2) %>% 
+  arrange(desc(total))
+
+duplicate <- data.frame(data)
+
+duplicate_data = data %>% 
+  group_by(total) %>%
+  summarise(count = n())  %>% 
+  arrange(desc(count))
+
+group_duplicate <- data.frame(duplicate_data)
+
+count <- dbGetQuery(sc, "Select count(ECIF) from ecif_golden_jdbc")
 
